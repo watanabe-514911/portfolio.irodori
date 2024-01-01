303d73157ae4fc98f3594985150b9436c1084cea
watanabe-514911 <145200157+watanabe-514911@users.noreply.github.com>
2024-01-01 10:21:43 +0900

    1/1

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
         var offset = $(this).offset().top;
         var scroll = $(window).scrollTop(); 
         var windowHeight = $(window).height();
-        if (scroll > offset - windowHeight + 50) {
+        if (scroll > offset - windowHeight + 150) {
           $(this).addClass("scroll-in");         
         }
       });
